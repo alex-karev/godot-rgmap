35e4cdba1d607986f0a49bc71e8bf59d767f938f
alex-karev <karev.as99@gmail.com>
2022-06-20 04:33:03 +0300

    Fix docs

---
+++ docs/classgodot_1_1RGTileset.js
@@ -0,0 +1,10 @@
+var classgodot_1_1RGTileset =
+[
+    [ "add_tile", "classgodot_1_1RGTileset.html#aa5b92c6b72b40222b9f73d92d6fb8500", null ],
+    [ "get_display_name", "classgodot_1_1RGTileset.html#a3cdc64c5bb22b19874edf5d9976f0dee", null ],
+    [ "get_index", "classgodot_1_1RGTileset.html#a1b3c551562afc0e9372aadd853273293", null ],
+    [ "get_name", "classgodot_1_1RGTileset.html#a3073336afb0daa60f6af96b4c9d3ba64", null ],
+    [ "get_tiles_count", "classgodot_1_1RGTileset.html#a8bdcde882a7a66b60f819b7187b79c9b", null ],
+    [ "is_passable", "classgodot_1_1RGTileset.html#ab196ccc0982354f30fdf0af0ec1ef92d", null ],
+    [ "is_transparent", "classgodot_1_1RGTileset.html#a62b71511b4f449fa2f8173b6b30030c4", null ]
+];
