7a57b8a7822b657b749c3466606513aad7add936
alex-karev <karev.as99@gmail.com>
2022-06-29 20:17:37 +0300

    Move doxygen.conf and doxygen theme to this branch

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-cd ../addons/godot-rgmap/docs
 doxygen doxygen.conf
-cd ../../../gh-pages
 git add .
 git commit -am "Update documentation"
 git push origin gh-pages
