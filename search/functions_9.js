48683a95db7ce49266ff16aed647234486ff6ed5
alex-karev <karev.as99@gmail.com>
2022-07-05 15:57:48 +0300

    Update documentation

--- search/functions_9.js
+++ search/functions_9.js
@@ -4,8 +4,8 @@ var searchData=
   ['raycast_5fvision_1',['raycast_vision',['../classgodot_1_1RGMap.html#af8078e95e12994ce402bd4a3880008db',1,'godot::RGMap']]],
   ['remove_5fentity_2',['remove_entity',['../classgodot_1_1RGMap.html#adf896538adc5e7bcd5f8cb584fe0cf5c',1,'godot::RGMap']]],
   ['remove_5fpathfinding_5fexception_3',['remove_pathfinding_exception',['../classgodot_1_1RGMap.html#a6ec099f81053cdd7782a9ccac45ea54d',1,'godot::RGMap']]],
-  ['request_5fchunks_5frender_4',['request_chunks_render',['../classgodot_1_1RGMap.html#af97b719797501ed8525d3aad37ed83d7',1,'godot::RGMap']]],
-  ['request_5fchunks_5fupdate_5',['request_chunks_update',['../classgodot_1_1RGMap.html#a2c565c4b9124368ebdaae851afd13585',1,'godot::RGMap']]],
+  ['request_5fchunks_5fload_4',['request_chunks_load',['../classgodot_1_1RGMap.html#a57499beedb8070d62924069c0d3b1a16',1,'godot::RGMap']]],
+  ['request_5fchunks_5frender_5',['request_chunks_render',['../classgodot_1_1RGMap.html#af97b719797501ed8525d3aad37ed83d7',1,'godot::RGMap']]],
   ['reset_5fchunk_6',['reset_chunk',['../classgodot_1_1RGMap.html#af16dce1ae917a6a42c237d7e7eebda92',1,'godot::RGMap']]],
   ['rpas_5fcalc_5fvisible_5fcells_5ffrom_7',['rpas_calc_visible_cells_from',['../classgodot_1_1RGMap.html#abc95ec5b234f69d9f372483f22b8f602',1,'godot::RGMap']]]
 ];
